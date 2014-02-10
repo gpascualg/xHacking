10a1780a2963ed9e4fdc4da214840d6e0f1a97f1
Blipi <gpascualg93@gmail.com>
2014-02-10 10:38:11 +0100

    Added some examples

---
+++ examples/CONSOLE_api.cpp
@@ -0,0 +1,44 @@
+#include <Windows.h>
+#include <stdio.h>
+
+// INCLUDE ORDER IS, **IS**, IMPORTANT!
+// If not done in this order, functions such as API::AutoLoad (depens on Loader) would not be available
+#include "xHacking.h"
+#include "Utilities/Utilities.h"
+#include "Loader/Loader.h"
+#include "API/api.h"
+#pragma comment(lib, "xHacking.lib")
+using namespace xHacking;
+
+int main()
+{
+	// Normal use case of an API call
+	GetAsyncKeyState(VK_F5);
+
+
+	// Hidden api call (without using xHacking)
+	typedef SHORT(WINAPI* get_t)(int);
+	get_t getAsync = (get_t)GetProcAddress(GetModuleHandle("user32.dll"), "GetAsyncKeyState");
+	getAsync(VK_F5);
+
+
+	// Hidden api call using xHacking
+	API<SHORT, int>* api = new API<SHORT, int>();
+	api->AutoLoad("user32.dll", "GetAsyncKeyState");
+	(*api)(VK_F5);
+	// Autoload will use the `Loader` class and wait until the DLL is loaded to get its data
+	// it can be used with the `->LoaderMode(API_LOADER_LOAD)` to force the DLL to be loaded (LoadLibrary).
+
+
+	// Hidden api call using xHacking + Fake
+	API<SHORT, int>* api = new API<SHORT, int>();
+	api->AutoFake(NULL)->AutoLoad("user32.dll", "GetAsyncKeyState");
+	(*api)(VK_F5);
+
+	// AutoFake requieres the module name (ie. fake call from "user32.dll" should be done by ->AutoFake("user32.dll"))
+	// NULL is to be used to fake call from the same module
+	// Autofake will modify the stack as `fake` the caller of the function, bypassing most checks
+	// The stack is also automatically cleaned up
+
+	return 0;
+}
---
+++ examples/DLL_detours.cpp
@@ -0,0 +1,121 @@
+// General
+#include <stdio.h>
+#include <Windows.h>
+
+// xHacking library
+// INCLUDE ORDER IS, **IS**, IMPORTANT!
+// If not done in this order, functions such as Detour::Wait (depens on Loader) would not be available
+#include "xHacking.h"
+#include "Utilities/Utilities.h"
+#include "Loader/Loader.h"
+#include "Detour/Detour.h"
+using namespace xHacking;
+
+#ifdef _DEBUG
+#pragma comment(lib, "xHacking_d.lib")
+#else
+#pragma comment(lib, "xHacking.lib")
+#endif
+
+// Variables which will hold the detours
+Detour<int, int, char*, int, int>* recvDetour = NULL;
+Detour<int, int, int, char*, int, int>* sendDetour = NULL;
+
+// Commented example, not used
+Detour<int, int, char*, int, int>* sendMemory = NULL;
+
+int WINAPI nuestro_recv(SOCKET s, char *buf, int len, int flags)
+{
+	// Calling the original function
+	int ret = (*recvDetour)(s, buf, len, flags);
+
+	__asm PUSHAD;
+	__asm PUSHFD;
+	
+	// Custom code
+
+	__asm POPFD;
+	__asm POPAD;
+
+	return ret;
+}
+
+// NOTE: unk0 is due to detouring 5 bytes from function start (as seen below)
+// and, probably, unk0 = EBP
+int WINAPI nuestro_send(int unk0, SOCKET s, char *buf, int len, int flags)
+{
+	__asm PUSHAD;
+	__asm PUSHFD;
+
+	// Custom code
+
+	__asm POPFD;
+	__asm POPAD;
+
+	return 0;
+	// We are in a trampoline, it doesn't really matter what we return
+}
+
+// This is in case we want to do a memory detour (PAGE_GUARD)
+int WINAPI nuestro_mem_send(int s, char* buf, int l, int f)
+{
+	__asm PUSHAD;
+	__asm PUSHFD;
+	
+	__asm POPFD;
+	__asm POPAD;
+
+	return 0;
+	// Again, it doesn't matter what we return, we are inside the VEH (error handler)
+}
+
+// This is asyncrouneusly (or sync if the DLL is already loaded) by the xHacking::Loader class
+void hookSend(Loader::Data* data)
+{
+	// We are performing the detour on +5 bytes, including a trampoline, so that's why we are doing
+	// it this way.
+	// It is, somehow, an example of a mid-function hooking
+	sendDetour = new Detour<int, int, int, char*, int, int>(data->Function + 5, (BYTE*)nuestro_send);
+	sendDetour->WithTrampoline(true)->Commit();
+
+	// A trampoline is basically some code to automatically return to the original program flow.
+
+	// Alternatively, we could apply a memory breakpoint uncommenting the below lines
+	// Both detours should not be used at the same time
+	//sendMemory = new Detour<int, int, char*, int, int>(data->Function + 5, (BYTE*)nuestro_mem_send);
+	//sendMemory->Type(DETOUR_MEM)->Commit();
+}
+
+void Hooks()
+{
+	// We first create the recv detour.
+	// We want it to be automatically created whenever WSOCK32 is loaded (and not load it),
+	// Thus, we use the `Wait` function in Detour.
+	// If we wanted to force load the DLL, we could use `Load`
+	
+	// int WINAPI recv(int, char*, int, int);
+	recvDetour = new Detour<int, int, char*, int, int>();
+	recvDetour->Wait("WSOCK32.dll", "recv", (BYTE*)nuestro_recv);
+
+	// We could specify the Detour type by calling `Type(DETOUR_X)` where DETOUR_X is defined in
+	// DETOUR_TYPE. That call should be done BEFORE the wait call, unexpected behaviour may happen
+	// otherwise
+		
+	// Here we use the Loader class to wait for the WS2_32 dll
+	// Once it is loaded, the function `hookSend` will be called
+	Loader::Wait("WS2_32.dll", "send", hookSend);
+}
+
+BOOL WINAPI DllMain(HINSTANCE instance, DWORD reason, DWORD reserved)
+{
+	if (reason == DLL_PROCESS_ATTACH)
+	{
+		// Use the xHacking::CreateConsole function
+		CreateConsole();
+
+		// Call our function
+		Hooks();
+	}
+
+	return true;
+}
