0fd723e806877c1c55336761b8a8bf486ddfa11a
Blipi <gpascualg93@gmail.com>
2014-02-10 11:51:03 +0100

    Remote: Added remote process opening code

---
+++ include/Remote/Remote.h
@@ -0,0 +1,39 @@
+#include "xHacking.h"
+
+#define XHACKING_REMOTE_START_NS namespace Remote {
+#define XHACKING_REMOTE_END_NS }
+
+XHACKING_START_NAMESPACE
+XHACKING_REMOTE_START_NS
+
+bool GrantPrivileges();
+DWORD FindProcessId(const std::string& processName);
+HANDLE Open(char* process);
+
+template<typename T> Read(HANDLE process, DWORD address)
+{
+	T value = 0;
+	size_t read = 0;
+	ReadProcessMemory(process, (LPCVOID)address, &value, sizeof(T), &read);
+
+	if (sizeof(T) != read)
+		NAMESPACE::SetLastError(REMOTE_READ_LENGTH_ERROR);
+
+	return value;
+}
+
+template<typename T> bool Write(HANDLE process, DWORD address, T value)
+{
+	WriteProcessMemory(process, address, &value, sizeof(T), &written);
+
+	if (written != sizeof(T))
+	{
+		NAMESPACE::SetLastError(REMOTE_WRITE_LENGTH_ERROR);
+		return false;
+	}
+
+	return true;
+}
+
+XHACKING_REMOTE_END_NS
+XHACKING_END_NAMESPACE
---
+++ src/Remote/Remote.cpp
@@ -0,0 +1,82 @@
+#include "Remote.h"
+#include <tlhelp32.h>
+
+// Disable BOOL forcing to true/false
+#pragma warning(disable: 4800)
+
+XHACKING_START_NAMESPACE
+XHACKING_REMOTE_START_NS
+
+bool GrantPrivileges()
+{
+	HANDLE hToken = NULL;
+	TOKEN_PRIVILEGES tokenPriv;
+	LUID luidDebug;
+	if (OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES, &hToken))
+	{
+		if (LookupPrivilegeValue(NULL, SE_DEBUG_NAME, &luidDebug))
+		{
+			tokenPriv.PrivilegeCount = 1;
+			tokenPriv.Privileges[0].Luid = luidDebug;
+			tokenPriv.Privileges[0].Attributes = SE_PRIVILEGE_ENABLED;
+			bool result = AdjustTokenPrivileges(hToken, FALSE, &tokenPriv, 0, NULL, NULL);
+			CloseHandle(hToken);
+
+			return result;
+		}
+	}
+
+	return false;
+}
+
+DWORD FindProcessId(const std::string& processName)
+{
+	PROCESSENTRY32 processInfo;
+	processInfo.dwSize = sizeof(processInfo);
+
+	HANDLE processesSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, NULL);
+	if (processesSnapshot == INVALID_HANDLE_VALUE)
+		return 0;
+
+	Process32First(processesSnapshot, &processInfo);
+	if (!processName.compare(processInfo.szExeFile))
+	{
+		CloseHandle(processesSnapshot);
+		return processInfo.th32ProcessID;
+	}
+
+	while (Process32Next(processesSnapshot, &processInfo))
+	{
+		if (!processName.compare(processInfo.szExeFile))
+		{
+			CloseHandle(processesSnapshot);
+			return processInfo.th32ProcessID;
+		}
+	}
+
+	CloseHandle(processesSnapshot);
+	return 0;
+}
+
+HANDLE Open(char* process)
+{
+	DWORD pID = FindProcessId(std::string(process));
+
+	if (!pID)
+	{
+		NAMESPACE::SetLastError(REMOTE_PROCESS_NOT_FOUND);
+		return 0;
+	}
+
+	DWORD access = PROCESS_ALL_ACCESS;
+	if (!GrantPrivileges())
+	{
+		NAMESPACE::SetLastError(REMOTE_GRANT_PRIVILEGES);
+		access = PROCESS_QUERY_INFORMATION | PROCESS_VM_READ | PROCESS_VM_WRITE;
+	}
+
+	return OpenProcess(PROCESS_ALL_ACCESS, FALSE, pID);
+}
+
+XHACKING_REMOTE_END_NS
+XHACKING_END_NAMESPACE
